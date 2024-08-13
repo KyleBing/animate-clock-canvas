c54483be84b1d779438db987fb5bf915773aa0e8
KyleBing <kylebing@163.com>
2024-08-13 11:57:20 +0800

    能正常显示时钟，但存在问题

--- animate-clock-canvas-browser.js
+++ animate-clock-canvas-browser.js
@@ -14,7 +14,7 @@ class AnimateClockCanvas {
      * @param bgColor 背景颜色
      */
     constructor( bgColor ) {
-        this.isPlaying = true // 默认自动播放
+        this.isPlayConstantly = true // 默认自动播放
 
         this.configFrame = {
             center: {
@@ -31,8 +31,6 @@ class AnimateClockCanvas {
             timeInit: new Date().getTime(),
             movement: 1,                           // 运动速度
         }
-        this.date = null
-
         this.init()
 
         window.onresize = () => {
@@ -45,25 +43,6 @@ class AnimateClockCanvas {
             let clockLayer = document.getElementById('clockLayer')
             this.updateFrameAttribute(clockLayer)
         }
-    }
-
-    play(){
-        if (this.isPlaying){
-
-        } else {
-            this.isPlaying = true
-            this.draw()
-        }
-    }
-    stop(){
-        this.isPlaying = false
-    }
-
-    moveDown(){
-        this.configClock.flowDirection = -1
-    }
-    moveUp(){
-        this.configClock.flowDirection = 1
     }
 
     updateFrameAttribute(clockLayer){
@@ -92,8 +71,6 @@ class AnimateClockCanvas {
         this.updateFrameAttribute(clockLayer)
         document.documentElement.append(clockLayer)
         this.configClock.timeLine =  0
-        this.date = new Date()
-
         this.draw()
     }
 
@@ -106,63 +83,133 @@ class AnimateClockCanvas {
         let contextClock = canvasClock.getContext('2d')
         contextClock.clearRect(0, 0, this.configFrame.width, this.configFrame.height)
 
-
         // 背景，没有 bgColor 的时候，背景就是透明的
         if (this.configFrame.bgColor){
             contextClock.fillStyle = this.configFrame.bgColor
-            contextClock.fillRect(0,0,this.configFrame.width, this.configFrame.height)
+            contextClock.fillRect(0, 0, this.configFrame.width, this.configFrame.height)
         }
 
-        // 居中
-        contextClock.translate(this.configFrame.center.x, this.configFrame.center.y)
-
-        this.drawClockPanel(contextClock, this.configFrame.center)
+        this.drawClockPanelHour(contextClock, this.configFrame.center)
+        this.drawClockPanelMinutes(contextClock, this.configFrame.center)
+        this.drawRefLines(contextClock, this.configFrame.center)
         this.drawCenter(contextClock, this.configFrame.center)
-        this.drawPointer(contextClock, this.configFrame.center)
+        this.drawPointerSecond(contextClock, this.configFrame.center)
+        this.drawPointerMinute(contextClock, this.configFrame.center)
+        this.drawPointerHour(contextClock, this.configFrame.center)
 
-        if (this.isPlaying) {
+
+        // show timeline
+        contextClock.fillStyle = 'black'
+        contextClock.font = '30px sans-serf'
+        contextClock.fillText(String(this.configClock.timeLine), 20 ,this.configFrame.height - 20)
+
+        if (this.isPlayConstantly) {
             window.requestAnimationFrame(() => {
                 this.draw()
             })
         }
     }
 
-    // 画时钟表盘
-    drawClockPanel(ctx, center){
+    drawRefLines(ctx, center){
+        const lineLength = 1000
+        ctx.save()
+        ctx.beginPath()
+        ctx.moveTo(center.x - lineLength/2, center.y)
+        ctx.lineTo(center.x + lineLength/2, center.y)
+        ctx.moveTo(center.x, center.y - lineLength/2)
+        ctx.lineTo(center.x, center.y + lineLength/2)
+        ctx.strokeStyle = 'magenta'
+        ctx.closePath()
+        ctx.stroke()
+        ctx.restore()
+    }
+
+    // 画时钟表盘: 时
+    drawClockPanelHour(ctx, center){
         const lineWidth = 10
         const lineHeight = 50
-        const offsetCenter = 200
+        const offsetCenter = 300
         ctx.save()
+        ctx.translate(center.x, center.y)
         for (let i = 0; i < 12; i++) {
-            ctx.rect(-lineWidth/2, offsetCenter, lineWidth, lineHeight)
-            ctx.rotate(Math.PI * 30 / 180)
+            ctx.rotate(Math.PI / 6)
+            ctx.rect(-lineWidth / 2, -offsetCenter, lineWidth, lineHeight)
         }
         ctx.fillStyle = '#2185ff'
-        ctx.fillText(this.configClock.timeLine, 0 ,this.configFrame.height)
-
         ctx.fill()
         ctx.restore()
     }
 
-
-    drawCenter(ctx, center){
+    // 画时钟表盘：分钟
+    drawClockPanelMinutes(ctx, center){
+        const lineWidth = 4
+        const lineHeight = 30
+        const offsetCenter = 300
         ctx.save()
-        ctx.arc(0,0,20,0,Math.PI * 2)
-        ctx.fillStyle = '#ff0064'
+        ctx.translate(center.x, center.y)
+        for (let i = 0; i < 60; i++) {
+            ctx.rotate(Math.PI / 30)
+            ctx.rect(-lineWidth / 2, -offsetCenter, lineWidth, lineHeight)
+        }
+        ctx.fillStyle = '#e7e7e7'
         ctx.fill()
+        ctx.restore()
     }
 
-    drawPointer(ctx, center){
-        const ms = this.date.getMilliseconds()
-        const seconds = this.date.getSeconds()
-        const rotateAngle = Math.PI * 2 * (ms/1000/60 + seconds/60)  // 秒 + 毫秒的角度
-        const lineWidth = 10
-        const lineHeight = 30
-        const offsetCenter = 100
+    drawCenter(ctx, center){
+        const centerRadius = 10
+        ctx.save()
+        ctx.translate(center.x, center.y)
+        ctx.arc(0, 0, centerRadius, 0, Math.PI * 2)
+        ctx.fillStyle = '#12ff00'
+        ctx.fill()
+        ctx.restore()
+    }
+
+    drawPointerSecond(ctx, center){
+        const ms = new Date().getMilliseconds()
+        const seconds = new Date().getSeconds()
+        const rotateAngle = Math.PI * 2 * (ms / 1000 / 60 + seconds / 60)  + Math.PI  // 秒 + 毫秒的角度
+        const lineWidth = 2
+        const lineHeight = 250
+        const offsetCenter = 50
+        ctx.save()
+        ctx.translate(center.x, center.y)
         ctx.rotate(rotateAngle)
         ctx.rect(-lineWidth/2, offsetCenter, lineWidth, lineHeight)
-        ctx.fillStyle = '#00ec1f'
+        ctx.fillStyle = '#ff0000'
         ctx.fill()
+        ctx.restore()
+    }
+
+    drawPointerMinute(ctx, center){
+        const seconds = new Date().getMinutes()
+        const rotateAngle = Math.PI * 2 * (seconds / 60) + Math.PI  // 秒 + 毫秒的角度
+        const lineWidth = 10
+        const lineHeight = 150
+        const offsetCenter = 50
+        ctx.save()
+        ctx.translate(center.x, center.y)
+        ctx.rotate(rotateAngle)
+        ctx.rect(-lineWidth/2, offsetCenter, lineWidth, lineHeight)
+        ctx.fillStyle = '#2185ff'
+        ctx.fill()
+        ctx.restore()
+    }
+
+    drawPointerHour(ctx, center){
+        const hours = new Date().getHours()
+        const rotateAngle = Math.PI * 2 * (hours / 12) + Math.PI // 秒 + 毫秒的角度
+        const lineWidth = 20
+        const lineHeight = 100
+        const offsetCenter = 50
+        ctx.save()
+        ctx.translate(center.x, center.y)
+        ctx.rotate(rotateAngle)
+        ctx.rect(-lineWidth/2, offsetCenter, lineWidth, lineHeight)
+        ctx.fillStyle = '#ff0000'
+        ctx.fill()
+        ctx.restore()
     }
 
 }
